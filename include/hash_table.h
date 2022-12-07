85930e5ecf3c636d46936ea9f639869ea876072f
logan <linhy@shanghaitech.edu.cn>
2022-12-07 20:57:51 +0800

    init commit

---
+++ include/hash_table.h
@@ -0,0 +1,13 @@
+//
+// Created by MARS on 2022/12/7.
+//
+
+#ifndef CUCKOO_HASHING_HASH_TABLE_H
+#define CUCKOO_HASHING_HASH_TABLE_H
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+
+
+#endif //CUCKOO_HASHING_HASH_TABLE_H
